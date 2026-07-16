6f483561eef657db52c9371a3a42e6668bbbc94f
priyankathakur06 <priyankathakur060801@gmail.com>
2026-07-16 21:50:15 +0530

    Fix CORS for Vercel

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://shop-frontend-seven-silk.vercel.app",
+    ],
     credentials: true,
   }),
 );
